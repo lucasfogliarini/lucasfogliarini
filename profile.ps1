a4cd34b00e0557d9d04864d94f8653c065e20be2
Lucas Fogliarini <lucasfogliarini@gmail.com>
2025-12-03 12:21:36 -0300

    Refactor aliases to functions in profile.ps1
    Updated PowerShell profile with function definitions for aliases.

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,17 @@
-Set-Alias k kubectl
-Set-Alias nodes kubectl get nodes -o wide
-Set-Alias pods kubectl get pods -o wide
-Set-Alias h helm
-Set-Alias d docker
-Set-Alias gs "git status"
+# ====================================================================
+#  PROFILE DO POWERSHELL - Lucas Fogliarini
+# ====================================================================
+#  Execute este comando para criar/carregar este profile se ele não existir:
+#  if (!(Test-Path $PROFILE)) { Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lucasfogliarini/lucasfogliarini/main/profile.ps1" -OutFile $PROFILE  } . $PROFILE Get-Alias
+# ====================================================================
+
+# ====================================================================
+#  Remover este profile completamente:
+#  Remove-Item -Path $PROFILE -Force
+
+function k { kubectl @args }
+function nodes { kubectl get nodes -o wide @args }
+function pods { kubectl get pods -o wide @args }
+function h { helm @args }
+function d { docker @args }
+function gs { git status @args }
