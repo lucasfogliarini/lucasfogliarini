03ff8f43e12fc078f126473549dd33f32f4d1808
Lucas Fogliarini <lucasfogliarini@gmail.com>
2025-12-15 19:21:06 -0300

    Update PowerShell profile loading command

--- profile.ps1
+++ profile.ps1
@@ -2,7 +2,7 @@
 #  PROFILE DO POWERSHELL - Lucas Fogliarini
 # ====================================================================
 #  Execute este comando para criar/carregar este profile forçado:
-#  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lucasfogliarini/lucasfogliarini/main/profile.ps1" -OutFile $PROFILE; . $PROFILE; Get-Command -CommandType Function | Where-Object { $_.Name -in @('nodes','pods','services', 'gs', 'd', 'k') }
+#  New-Item -ItemType File -Path $PROFILE -Force | Out-Null; Invoke-WebRequest "https://raw.githubusercontent.com/lucasfogliarini/lucasfogliarini/main/profile.ps1" -OutFile $PROFILE; . $PROFILE; Get-Command -CommandType Function | Where-Object { $_.Name -in @('nodes','pods','services','gs','d','k') }
 # ====================================================================
 
 function nodes { kubectl get nodes -o wide @args }
